3a117c680481083c0db5038948a129788cde4eea
ariya das <ariyadas33@gmail.com>
2023-06-15 16:29:08 +0530

    cart sreen initialized

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ import connectDatabase from "./db.js";
 import express from "express";
 
 import productRoutes from "./routes/productRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 connectDatabase();
 const app = express();
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
+// app.use("/api/users", userRoutes);
 
 console.log(process.env.PORT);
 const port = process.env.PORT || 5000;
